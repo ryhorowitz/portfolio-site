79ddfaab478d992bb5bc82a2e0a5eafda212c848
ryan horowitz <ryan.w.horowitz@gmail.com>
2023-10-13 11:18:28 -0400

    imported side bar template

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './components/Sidebar'
+import Sidebar from './components/Sidebar';
 function App() {
   return (
     <div className="App">
       Hello World
+      <Sidebar />
     </div>
   );
 }
