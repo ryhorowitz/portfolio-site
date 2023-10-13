c3bf4e7d5350fa46c795cfc1bd0181b76bbca965
ryan horowitz <ryan.w.horowitz@gmail.com>
2023-10-13 14:36:19 -0400

    add bootstrap scripts and styling to index.html

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,3 +1,4 @@
+import ContentContainer from "./ContentContainer"
 function Sidebar() {
 
   return (
@@ -27,66 +28,22 @@ function Sidebar() {
                   </li>
                 </ul>
               </li>
+
               <li>
                 <a href="#" class="nav-link px-0 align-middle">
-                  <i class="fs-4 bi-table"></i> <span class="ms-1 d-none d-sm-inline">Orders</span></a>
-              </li>
-              <li>
-                <a href="#submenu2" data-bs-toggle="collapse" class="nav-link px-0 align-middle ">
-                  <i class="fs-4 bi-bootstrap"></i> <span class="ms-1 d-none d-sm-inline">Bootstrap</span></a>
-                <ul class="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
-                  <li class="w-100">
-                    <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 1</a>
-                  </li>
-                  <li>
-                    <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Item</span> 2</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a href="#submenu3" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
-                  <i class="fs-4 bi-grid"></i> <span class="ms-1 d-none d-sm-inline">Products</span> </a>
-                <ul class="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
-                  <li class="w-100">
-                    <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 1</a>
-                  </li>
-                  <li>
-                    <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 2</a>
-                  </li>
-                  <li>
-                    <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 3</a>
-                  </li>
-                  <li>
-                    <a href="#" class="nav-link px-0"> <span class="d-none d-sm-inline">Product</span> 4</a>
-                  </li>
-                </ul>
-              </li>
-              <li>
-                <a href="#" class="nav-link px-0 align-middle">
-                  <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Customers</span> </a>
+                  <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Contact</span> </a>
               </li>
             </ul>
             <hr></hr>
-            <div class="dropdown pb-4">
-              <a href="#" class="d-flex align-items-center text-white text-decoration-none dropdown-toggle" id="dropdownUser1" data-bs-toggle="dropdown" aria-expanded="false">
-                <img src="https://github.com/mdo.png" alt="hugenerd" width="30" height="30" class="rounded-circle" />
-                <span class="d-none d-sm-inline mx-1">loser</span>
+            <div class="pb-4">
+              <a href="#" class="d-flex align-items-center text-white text-decoration-none" id="User1" aria-expanded="false">
+                <img src="https://media.licdn.com/dms/image/C4E35AQFcE_PnCoPaUQ/profile-framedphoto-shrink_100_100/0/1601911718102?e=1697828400&v=beta&t=PgJQ_4ME4Ep_oENbNhG-NWec6GC4HEo3mWPER2wjjVQ" alt="" width="40" height="40" class="rounded-circle" />
+                <span class="d-none d-sm-inline mx-1">Ryan Horowitz</span>
               </a>
-              <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
-                <li><a class="dropdown-item" href="#">New project...</a></li>
-                <li><a class="dropdown-item" href="#">Settings</a></li>
-                <li><a class="dropdown-item" href="#">Profile</a></li>
-                <li>
-                  <hr class="dropdown-divider"></hr>
-                </li>
-                <li><a class="dropdown-item" href="#">Sign out</a></li>
-              </ul>
             </div>
           </div>
         </div>
-        <div class="col py-3">
-          Content area...
-        </div>
+        <ContentContainer />
       </div>
     </div>
   )
