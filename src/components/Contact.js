a0d364f7f6ee740e4ed6ba3850d47cdd4e7d59bc
ryan horowitz <ryan.w.horowitz@gmail.com>
2023-10-16 15:40:08 -0400

    did some styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -19,24 +19,29 @@ function Contact() {
   return (
     <>
 
-      <h1>Contact </h1>
+      <h1 className="px-2 text-center">Contact </h1>
       <br></br>
       {/* <div className="col-lg"> */}
-      <ul className="list-unstyled list-group px-5">
-        <li className="list-group-item list-group-item-secondary py-3">Email: ryan.w.horowitz@gmail.com</li>
-        <li className="list-group-item list-group-item-secondary py-3">Phone: 917 584-0900</li>
-        <li className="list-group-item list-group-item-secondary py-3">Blogs: <a href="https://medium.com/@ryan.w.horowitz">medium.com/@ryan.w.horowitz</a></li>
+      <ul className="list-unstyled list-group px-5 mx-5">
+        <li className="list-group-item list-group-item-secondary py-3">Email:
+          <span className="fw-semibold"> ryan.w.horowitz@gmail.com</span>
+        </li>
+        <li className="list-group-item list-group-item-secondary py-3">Phone:
+          <span className="fw-semibold"> (917) 584-0900</span>
+        </li>
+        <li className="list-group-item list-group-item-secondary py-3">Blogs:
+          <span className="fw-semibold">
+            <a href="https://medium.com/@ryan.w.horowitz">medium.com/@ryan.w.horowitz</a>
+          </span>
+        </li>
         <li className="list-group-item list-group-item-secondary py-3">
+          <h5>For Resume please see my LinkedIn profile below</h5>
           <div className="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="light" data-type="HORIZONTAL" data-vanity="ryan-horowitz" data-version="v1">
-            <a className="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/ryan-horowitz?trk=profile-badge">
-
-            </a>
+            <a className="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/ryan-horowitz?trk=profile-badge"> </a>
           </div>
         </li>
       </ul>
       {/* </div> */}
-
-
     </>
   )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,14 +3,16 @@ function Home() {
     <>
       <div className="container-fluid">
 
-        <h1>Home</h1>
-        <div className="row">
+        <h1 className="px-2 text-center">Ryan Horowitz</h1>
+        <div className="row pt-2">
           <div className="col-6">
             <img className="img-fluid" src="https://portfolio-public-resources.s3.amazonaws.com/headshot.jpeg" alt="Profile Pic"></img>
 
           </div>
           <div className="col-6">
-            <h1>REsume will go here</h1>
+            <h2 className="text-center">Bio blerb will go here About:
+
+            </h2>
           </div>
         </div>
       </div>
