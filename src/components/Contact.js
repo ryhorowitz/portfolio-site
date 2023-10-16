2a58f33312cf8a25d7311e4a1e1c81ef34bd13ba
ryan horowitz <ryan.w.horowitz@gmail.com>
2023-10-16 15:04:14 -0400

    added profile pic

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -21,7 +21,11 @@ function Contact() {
 
       <h1>Contact </h1>
       <br></br>
-      <ul className="list-unstyled list-group">
+      {/* <div className="col-lg"> */}
+      <ul className="list-unstyled list-group px-5">
+        <li className="list-group-item list-group-item-secondary py-3">Email: ryan.w.horowitz@gmail.com</li>
+        <li className="list-group-item list-group-item-secondary py-3">Phone: 917 584-0900</li>
+        <li className="list-group-item list-group-item-secondary py-3">Blogs: <a href="https://medium.com/@ryan.w.horowitz">medium.com/@ryan.w.horowitz</a></li>
         <li className="list-group-item list-group-item-secondary py-3">
           <div className="badge-base LI-profile-badge" data-locale="en_US" data-size="large" data-theme="light" data-type="HORIZONTAL" data-vanity="ryan-horowitz" data-version="v1">
             <a className="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/ryan-horowitz?trk=profile-badge">
@@ -29,9 +33,9 @@ function Contact() {
             </a>
           </div>
         </li>
-        <li className="list-group-item list-group-item-secondary py-3">Email: ryan.w.horowitz@gmail.com</li>
-        <li className="list-group-item list-group-item-secondary py-3">Phone: 917 584-0900</li>
       </ul>
+      {/* </div> */}
+
 
     </>
   )
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,19 @@
 function Home() {
   return (
     <>
-      <h1>Home</h1>
+      <div className="container-fluid">
+
+        <h1>Home</h1>
+        <div className="row">
+          <div className="col-6">
+            <img className="img-fluid" src="https://portfolio-public-resources.s3.amazonaws.com/headshot.jpeg" alt="Profile Pic"></img>
+
+          </div>
+          <div className="col-6">
+            <h1>REsume will go here</h1>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
